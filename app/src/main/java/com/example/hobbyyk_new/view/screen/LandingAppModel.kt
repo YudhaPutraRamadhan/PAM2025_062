f958a289ce9f9d2d5e001062095d7be454d45297
YudhaPutraRamadhan <yudharamadhan838@gmail.com>
2025-12-24 11:15:09 +0700

    Membuat LandingAppViewModel untuk mengecek sesi user

---
+++ app/src/main/java/com/example/hobbyyk_new/view/screen/LandingAppModel.kt
@@ -0,0 +1,44 @@
+package com.example.hobbyyk_new.view.screen
+
+import android.app.Application
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hobbyyk_new.data.api.RetrofitClient
+import com.example.hobbyyk_new.data.datastore.UserStore
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class LandingAppViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val userStore = UserStore(application)
+    var startDestination by mutableStateOf<String?>(null)
+
+    init {
+        checkSession()
+    }
+
+    private fun checkSession() {
+        viewModelScope.launch {
+            delay(2000)
+
+            val token = userStore.authToken.first()
+            val role = userStore.userRole.first()
+
+            if (!token.isNullOrEmpty()) {
+                RetrofitClient.authToken = token
+
+                if (role == "super_admin" || role == "admin") {
+                    startDestination = "super_admin_dashboard"
+                } else {
+                    startDestination = "home"
+                }
+            } else {
+                startDestination = "login"
+            }
+        }
+    }
+}
